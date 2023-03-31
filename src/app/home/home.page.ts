b483264c66ec79f9b429f62109602d9716d2580b
sfelipecarl36 <feliperara2000@hotmail.com>
2023-03-31 16:55:31 -0300

    slideshow start and categorias create

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AuthService } from '../shared/auth-service';
-import { User } from '../shared/user';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +11,25 @@ import { User } from '../shared/user';
 })
 export class HomePage {
   user: any;
+  categorias: any;
+  localCat: any;
+
 
   slideOpts = { 
     initialSlide: 1, 
     slidesPerView: 2.4,
     speed: 350, 
     effect: 'flip', 
+    spaceBetween: 20
     }; 
+
+  slideOptsCat = { 
+      initialSlide: 1, 
+      slidesPerView: 5,
+      speed: 350, 
+      effect: 'flip', 
+      spaceBetween: 12
+      }; 
 
   constructor(
     private auth: AuthService,
@@ -27,6 +39,22 @@ export class HomePage {
         this.auth.GuardLogin();
         this.user = this.auth.userData;
         this.user['name'] = this.user['displayName'].substring(0, this.user['displayName'].indexOf(' '));
+        
+        this.categorias = this.firestore.collection('categorias').valueChanges();
+
+        interface Categoria {
+          img: any,
+          uid: any,
+          nome: any
+        }
+
+        this.categorias.subscribe((res: Categoria[]) => {
+        
+          res.forEach((cat) => {
+          console.log(cat);
+          });   
+        })
+
   }
 
   async logout(){
